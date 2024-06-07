d749e50eeb9d96a1ec0a4fc6b2487816d8f6aa30
abdulmukit98 <mdabdulmukit98@gmail.com>
2024-06-07 18:24:29 +0600

    Api resolved

--- users_app/lib/global/global_var.dart
+++ users_app/lib/global/global_var.dart
@@ -5,11 +5,9 @@ String userName = "";
 String userPhone = "";
 String userID = FirebaseAuth.instance.currentUser!.uid;
 
-// String googleMapKey = "AIzaSyBfd5ObQjgWZWThSwvD1lt4yngpgqaTBOk";
 String googleMapKey = "API KEY";
-String serverKeyFCM = "key=AAAAFSDjHpo:APA91bEO-3hmIwkOqjPj1UZziSh0rFQi0Qfi2Bil6Wol6Tgald5nHyU1VSC1Qzw8yZUAK_fILfiI6h2NTOCB8OxsxPrEQHv7CX4R55fGoWoz6NxORUOLetLMXBKTHm_XN7AJXb0Y3P9T";
+String serverKeyFCM = "key=API KEY";
 String geocodingApiKey = "API Key";
-// String placesAPI = "AIzaSyBfd5ObQjgWZWThSwvD1lt4yngpgqaTBOk";
 String placesAPI = "API KEY";
 
 const CameraPosition googlePlexInitialPosition = CameraPosition(
